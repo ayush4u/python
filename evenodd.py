72b3482bf8a2336cec739fb7ca0496559c592e76
ayush <59681649+ayush4u@users.noreply.github.com>
2020-11-16 09:23:32 +0530

    Delete evenodd.py

--- evenodd.py
+++
@@ -1,7 +0,0 @@
-a=int(input("enter the starting number:"))
-b=int(input("enter the ending number:"))
-for x in range(a,b):
-    if x%2==0:
-        print("{} is even".format(x))
-    else:
-        print("{} is odd".format(x))
